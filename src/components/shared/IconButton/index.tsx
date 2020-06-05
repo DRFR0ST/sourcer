3548c9638b2f5f4c78583e2181c5647b69c10b9b
Mike Eling <mike.eling97@gmail.com>
2020-06-06 00:37:58 +0200

    :wrench: Pass click event handler to IconButton

--- src/components/shared/IconButton/index.tsx
+++ src/components/shared/IconButton/index.tsx
@@ -3,18 +3,19 @@ import styles from "./styles";
 import { createUseStyles } from "react-jss";
 
 type TIconButtonProps = {
-    name: string
+    name: string;
+    onClick: () => void
 }
 
 // Creates hook composing styles.
 const useStyles = createUseStyles(styles);
 
-const IconButton = ({ name }: TIconButtonProps) => {
+const IconButton = ({ name, onClick }: TIconButtonProps) => {
     const classes = useStyles();
     // TODO: Add styling.
     // TODO: Implement icons.
     // TODO: Just finish the thang..
-    return <button className={ classes.root }><span>{name}</span></button>
+    return <button onClick={onClick} className={ classes.root }><span>{name}</span></button>
 }
 
 export default IconButton;
